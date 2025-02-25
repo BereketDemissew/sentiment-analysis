4e5442c3cdca182e5fd0a70fa3328987f78aba56
BobTheProdigy <118394684+BobTheProdigy@users.noreply.github.com>
2025-02-25 00:05:38 -0500

    Update trainModel.py

--- trainModel.py
+++ trainModel.py
@@ -2,22 +2,20 @@ import torch
 import torch.nn as nn
 
 # File imports
-import calculateAccuracy
-import makeLinearGraphs
-import checkpoints
+import sendEmail
 
-def training_loop(model, optimizer, vocab, padding, updated_score, val_review, val_score, test_review, test_score, trainingLosses, validationLosses, previousTrainingLoops, device):
+def training_loop(model, optimizer, padding, updated_score, val_review, val_score, trainingLosses, validationLosses, previousTrainingLoops, device):
     #training loop
     batch_size = 512
     loss_function = nn.BCELoss()
-    loops = 10
+    loops = 1
     # trainingLosses = []
     # validationLosses = []
     val_batch = int(batch_size * 0.8)
     print(f'beginning training {loops} loops')
     for epoch in range(loops):
 
-        randperm = torch.randperm(len(padding))
+        randperm = torch.randperm(len(padding)) # so model recognizes patterns from batch across the board and not each specific batch
         padding, updated_score = padding[randperm].to(device), updated_score[randperm].to(device)
         model.train()
         total_loss = 0
@@ -52,17 +50,15 @@ def training_loop(model, optimizer, vocab, padding, updated_score, val_review, v
                 loss_val = loss_function(pred, mini_batch_labels_val)
                 val_loss += loss_val.item()
             validationLosses.append(val_loss / (len(val_review) // val_batch))
-
-        if (epoch % 10 == 9):
-          print(f"{epoch + 1 + previousTrainingLoops} epochs completed -- training loss: {round(trainingLosses[-1], 2)} -- validation loss: {round(validationLosses[-1], 2)}")
+        print('loops val:', loops)
+        if loops != 0 and loops // 20 != 0: # avoid ZeroDivisionError when running 0 extra loops
+            print('enters as loops != 0 and loops // 20 != 0')
+            if ((epoch + 1) % (loops // 20) == 9):
+                print(f"{epoch + 1 + previousTrainingLoops} epochs completed -- training loss: {round(trainingLosses[-1], 2)} -- validation loss: {round(validationLosses[-1], 2)}")
+                sendEmail.sendEmail(epoch + 1, round(trainingLosses[-1], 2), round(validationLosses[-1], 2), False, "", accuracy = 0.0)
     checkpoint = {
-    # 'model': model.state_dict(),
-    # 'optimizer': optimizer.state_dict(),
     'loops': loops + previousTrainingLoops,
     'trainingLosses': trainingLosses,
     'validationLosses': validationLosses
     }
-    
-    # if lowest is below 0.4 change how it works for later
-    # return checkpoint
     return checkpoint

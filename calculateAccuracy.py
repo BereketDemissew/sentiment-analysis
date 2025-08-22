58726edee521178638c03ed6e378c18973bd50c4
BobTheProdigy <118394684+BobTheProdigy@users.noreply.github.com>
2025-08-22 11:12:55 -0400

    Update calculateAccuracy.py
    Avoids making a random permutation in each minibatch.

--- calculateAccuracy.py
+++ calculateAccuracy.py
@@ -1,27 +1,27 @@
 import torch
 import numpy as np
 
-def compute_accuracy(model, test_data, test_labels, epochs, device):
+def compute_accuracy(model, test_data, test_labels, device, batchSize):
     model.eval()
-    batch_size = int(512 * .8)
     accuracyList = []
-    correct = []
+    
     with torch.no_grad():
-        for i in range(epochs):
-            randperm = torch.randperm(len(test_data))
-            test_data, test_labels = test_data[randperm], test_labels[randperm]
-            model.eval()
-            for i in range(0, len(test_data), batch_size):
-                mini_batch = test_data[i:i + batch_size].to(device)
-                mini_batch_labels = test_labels[i:i + batch_size].to(device)
-                pred = model(mini_batch)
-                predictions = torch.round(pred)
-                correct = torch.sum(predictions == mini_batch_labels)
-
-                accuracy = correct.item()/predictions.size(0) *100
-                accuracyList.append(accuracy)
-    accuracyList = np.array(accuracyList)
+        for i in range(0, len(test_data), batchSize):
+            # randperm = torch.randperm(len(test_data)) # since testing accuracy on whole set, don't need to do randperm
+            # test_data, test_labels = test_data[randperm], test_labels[randperm]
+            mini_batch = test_data[i : i+batchSize].to(device)
+            mini_batch_labels = test_labels[i : i+batchSize].to(device)
+            
+            
+            logits = model(mini_batch)                   # raw outputs
+            probabilities = torch.sigmoid(logits)        # convert to [0,1]
+            predictions = (probabilities > 0.5).long()   # threshold at 0.5
+            
+            correct = torch.sum(predictions.view(-1) == mini_batch_labels.view(-1))
+            accuracy = correct.item()/predictions.size(0) # *100, want accuracy to be decimal to fit on loss graph
+            accuracyList.append(accuracy)
+    accuracyList = np.mean(accuracyList)
     accuracy = accuracyList.mean()
 
-    print(f"accuracy = {accuracy:.2f}%")
+    print(f"accuracy = {accuracy*100:.2f}%")
     return accuracy
